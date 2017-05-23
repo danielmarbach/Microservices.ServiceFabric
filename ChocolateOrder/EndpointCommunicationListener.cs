9538c81e51bab5353db0bf8f6fe4d866b88192e6
danielmarbach <daniel.marbach@openplace.net>
2017-05-23 23:00:35 +0200

    ChocolateOrder service hooked up to ASB

---
+++ ChocolateOrder/EndpointCommunicationListener.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using NServiceBus;
+
+namespace ChocolateOrder
+{
+    public class EndpointCommunicationListener :
+        ICommunicationListener
+    {
+        StatefulServiceContext context;
+        IReliableStateManager stateManager;
+        IEndpointInstance endpointInstance;
+        EndpointConfiguration endpointConfiguration;
+
+        public EndpointCommunicationListener(StatefulServiceContext context, IReliableStateManager stateManager)
+        {
+            this.context = context;
+            this.stateManager = stateManager;
+        }
+
+        public async Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+            Logger.Log = m => ServiceEventSource.Current.ServiceMessage(context, m);
+
+            var partitionInfo = await ServicePartitionQueryHelper
+                .QueryServicePartitions(context.ServiceName, context.PartitionId)
+                .ConfigureAwait(false);
+
+            endpointConfiguration = new EndpointConfiguration("chocolateorder");
+
+            var transport = endpointConfiguration.ApplyCommonConfiguration(stateManager);
+
+            ConfigureLocalPartitionsChocolateOrder(endpointConfiguration, partitionInfo);
+
+            return null;
+        }
+
+        static void ConfigureLocalPartitionsChocolateOrder(EndpointConfiguration endpointConfiguration,
+            PartitionsInformation partitionInfo)
+        {
+            endpointConfiguration.RegisterPartitionsForThisEndpoint(
+                localPartitionKey: partitionInfo.LocalPartitionKey,
+                allPartitionKeys: partitionInfo.Partitions);
+        }
+
+        public async Task Run()
+        {
+            if (endpointConfiguration == null)
+            {
+                var message =
+                    $"{nameof(EndpointCommunicationListener)} Run() method should be invoked after communication listener has been opened and not before.";
+
+                Logger.Log(message);
+                throw new Exception(message);
+            }
+
+            endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            return endpointInstance.Stop();
+        }
+
+        public void Abort()
+        {
+            // Fire & Forget Close
+            CloseAsync(CancellationToken.None);
+        }
+    }
+}
---
+++ ChocolateOrder/ServicePartitionQueryHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Fabric;
+using System.Linq;
+using System.Threading.Tasks;
+
+public static class ServicePartitionQueryHelper
+{
+    public static async Task<PartitionsInformation> QueryServicePartitions(Uri serviceName, Guid partitionId)
+    {
+        using (var client = new FabricClient())
+        {
+            var servicePartitionList = await client.QueryManager.GetPartitionListAsync(serviceName)
+                .ConfigureAwait(false);
+
+            var partitionInformations =
+                servicePartitionList.Select(x => new {
+                    PartitionKey = x.PartitionInformation.Kind == ServicePartitionKind.Named
+                        ? ((NamedPartitionInformation)x.PartitionInformation).Name
+                        : ((Int64RangePartitionInformation)x.PartitionInformation).HighKey.ToString(),
+                    PartitionId = x.PartitionInformation.Id
+                }).ToList();
+
+            return new PartitionsInformation
+            {
+                LocalPartitionKey = partitionInformations.Single(p => p.PartitionId == partitionId).PartitionKey,
+                Partitions = partitionInformations.Select(p => p.PartitionKey).ToArray()
+            };
+        }
+    }
+}
+
+public class PartitionsInformation
+{
+    public string LocalPartitionKey { get; set; }
+    public string[] Partitions { get; set; }
+}

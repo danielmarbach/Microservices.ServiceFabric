ca4c799ea00f3e5304e25181bdc6e24cf1e23aa5
danielmarbach <daniel.marbach@openplace.net>
2017-05-24 10:44:02 +0200

    OrderProcessManager

---
+++ ChocolateOrder/OrderProcessManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Contracts;
+using NServiceBus;
+
+namespace ChocolateOrder
+{
+    public class OrderProcessManager : Saga<OrderProcessManager.OrderProcessData>
+        , IAmStartedByMessages<OrderChocolate>,
+        IHandleTimeouts<OrderProcessManager.BuyersRemorsePeriodOver>
+    {
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<OrderProcessData> mapper)
+        {
+            mapper.ConfigureMapping<OrderChocolate>(m => m.OrderId)
+                .ToSaga(s => s.OrderId);
+        }
+
+        public Task Handle(OrderChocolate message, IMessageHandlerContext context)
+        {
+            Logger.Log($"##### OrderProcessManager saga for {Data.OrderId} removerse period started.");
+
+            return RequestTimeout<BuyersRemorsePeriodOver>(context, DateTime.UtcNow.AddSeconds(5));
+        }
+
+        public Task Timeout(BuyersRemorsePeriodOver state, IMessageHandlerContext context)
+        {
+            Logger.Log($"##### OrderProcessManager saga for {Data.OrderId} buyers remorse period over.");
+
+            return Task.CompletedTask;
+        }
+
+        public class OrderProcessData :
+            ContainSagaData
+        {
+            public Guid OrderId { get; set; }
+        }
+
+        public class BuyersRemorsePeriodOver
+        {
+        }
+    }
+}
--- Contracts/OrderChocolate.cs
+++ Contracts/OrderChocolate.cs
@@ -1,9 +1,16 @@
-﻿using NServiceBus;
+﻿using System;
+using NServiceBus;
 
 namespace Contracts
 {
     public class OrderChocolate : ICommand
     {
+        public OrderChocolate()
+        {
+            OrderId = Guid.NewGuid();
+        }
+
         public string ChocolateType { get; set; }
+        public Guid OrderId { get; }
     }
 }

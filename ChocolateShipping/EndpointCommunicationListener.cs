24d9822bd9d3b5c9f832fefe47e2c23cfac2ad66
danielmarbach <daniel.marbach@openplace.net>
2017-05-24 12:45:13 +0200

    OrderShipping reroutes commands on the receiver side

--- ChocolateShipping/EndpointCommunicationListener.cs
+++ ChocolateShipping/EndpointCommunicationListener.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Fabric;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Contracts;
 using Microsoft.ServiceFabric.Data;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using NServiceBus;
@@ -37,6 +39,7 @@ namespace ChocolateShipping
             var transport = endpointConfiguration.ApplyCommonConfiguration(stateManager, servicePartitionInformation);
 
             ConfigureLocalPartitionsChocolateShipping(endpointConfiguration, partitionInfo);
+            ConfigureReceiverSideDistributionShipOrder(transport, partitionInfo);
 
             return null;
         }
@@ -47,6 +50,35 @@ namespace ChocolateShipping
             endpointConfiguration.RegisterPartitionsForThisEndpoint(
                 localPartitionKey: partitionInfo.LocalPartitionKey,
                 allPartitionKeys: partitionInfo.Partitions);
+        }
+
+        static void ConfigureReceiverSideDistributionShipOrder(TransportExtensions<AzureServiceBusTransport> transportConfig, PartitionsInformation partitionInfo)
+        {
+            #region ConfigureReceiverSideDistribution-CandidateVoteCount
+
+            var partitionKeysAsInts = partitionInfo.Partitions.Select(i => new { Key = i, AsInt = Convert.ToInt32(i) }).OrderBy(i => i.AsInt);
+
+            var routing = transportConfig.Routing();
+            var receiverSideDistribution = routing.EnableReceiverSideDistribution(partitionInfo.Partitions);
+            receiverSideDistribution.AddPartitionMappingForMessageType<ShipOrder>(
+                mapMessageToPartitionKey: shipOrder =>
+                {
+                    var zipCodeAsNumber = Convert.ToInt32(shipOrder.ZipCode);
+                    int current = -1;
+                    foreach (var keys in partitionKeysAsInts)
+                    {
+                        var next = keys.AsInt;
+                        if (zipCodeAsNumber > current && zipCodeAsNumber <= next)
+                        {
+                            return keys.Key;
+                        }
+
+                        current = next;
+                    }
+                    throw new Exception($"Invalid zip code '{zipCodeAsNumber}' for message of type '{shipOrder.GetType()}'.");
+                });
+
+            #endregion
         }
 
         public async Task Run()

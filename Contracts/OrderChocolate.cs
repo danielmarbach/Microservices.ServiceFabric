577d211100387f28e0bd43a30aece4c77c060ccf
danielmarbach <daniel.marbach@openplace.net>
2017-05-29 15:26:17 +0200

    Moved out order id

--- Contracts/OrderChocolate.cs
+++ Contracts/OrderChocolate.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NServiceBus;
+﻿using NServiceBus;
 
 namespace Contracts
 {
@@ -13,35 +12,5 @@ namespace Contracts
 
         public string ChocolateType { get; }
         public string OrderId { get; }
-    }
-
-    public struct OrderId
-    {
-        private OrderId(Guid orderId, string chocolateType)
-        {
-            Id = orderId;
-            ChocolateType = chocolateType;
-        }
-
-        public OrderId(string chocolateType)
-        {
-            Id = Guid.NewGuid();
-            ChocolateType = chocolateType;
-        }
-
-        public Guid Id { get; }
-
-        public string ChocolateType { get; }
-
-        public static implicit operator string(OrderId id)
-        {
-            return $"{id.ChocolateType};{id.Id}";
-        }
-
-        public static implicit operator OrderId(string id)
-        {
-            var split = id.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
-            return new OrderId(Guid.Parse(split[1]), split[0]);
-        }
     }
 }

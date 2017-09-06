b6d989bba5c596c246f4f7c66fe8e3545a97bb19
danielmarbach <daniel.marbach@openplace.net>
2017-09-06 10:14:20 +0200

    Added RunAsync

--- OrderChocolate/OrderChocolate.cs
+++ OrderChocolate/OrderChocolate.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Fabric;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
@@ -18,6 +20,20 @@ namespace OrderChocolate
         protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
         {
             return new[]{ new ServiceInstanceListener(this.CreateServiceRemotingListener) };
+        }
+
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            long iterations = 0;
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                ServiceEventSource.Current.ServiceMessage(this.Context, $"Working-{Context.InstanceId}-{++iterations}");
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
         }
 
         // TODO 4
